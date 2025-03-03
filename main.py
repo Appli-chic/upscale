732939868bffd8e7d90609eba776cb9c4cf4baec
Guillaume Belouin <gbelouin@protonmail.com>
2025-03-03 14:28:55 +0100

    Update requirements.txt with Flask

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from aura_sr import AuraSR
 import requests
 from io import BytesIO
 from PIL import Image
+from flask import Flask
 
 aura_sr = AuraSR.from_pretrained("fal/AuraSR-v2")
 
-def load_image_from_url(url):
-    response = requests.get(url)
-    image_data = BytesIO(response.content)
-    return Image.open(image_data)
+# def load_image_from_url(url):
+#     response = requests.get(url)
+#     image_data = BytesIO(response.content)
+#     return Image.open(image_data)
+#
+# image = load_image_from_url("https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg")
+# upscaled_image = aura_sr.upscale_4x_overlapped(image)
+#
+# upscaled_image.save("upscaled_image.png")
 
-image = load_image_from_url("https://www.happybrainscience.com/wp-content/uploads/2017/07/derwent-morning-Cropped.jpg")
-upscaled_image = aura_sr.upscale_4x_overlapped(image)
+app = Flask(__name__)
 
-upscaled_image.save("upscaled_image.png")
+@app.route("/")
+def upscale_image():
+    return "test"
